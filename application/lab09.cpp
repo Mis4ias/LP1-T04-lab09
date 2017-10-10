0205a8a717aecba3b0c581f8e60523cebf3cf57e
Misael Batista Damasceno <mbd@A307-21L.imd.local>
2017-10-10 10:35:13 -0300

    main

--- application/lab09.cpp
+++ application/lab09.cpp
@@ -4,37 +4,32 @@
 #include <sstream>
 using namespace std;
 
-int main(int argc,char argv*[]){	
-	vector<unsigned> input;
+int main(int argc,char *argv[]){	
+	vector<unsigned> input = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	Sort <unsigned> obj(&input);
+	/*
 	if(argv[1]=="--insertion"){
-		getline(cin,input);
-		Sort <unsigned> obj(&input);
+		
 		obj.print_vtr(input);
 		obj.insertion(input);
 		obj.print_vtr(input);
 	}
 	if(argv[1]=="--selection"){
-		getline(cin,input);
-		Sort <unsigned> obj(&input);
 		obj.print_vtr(input);
 		obj.selection(input);
 		obj.print_vtr(input);
 	}
 	if(argv[1]=="--quick"){
-		getline(cin,input);
-		Sort <unsigned> obj(&input);
 		obj.print_vtr(input);
 		obj.quick(input);
 		obj.print_vtr(input);
 	}
 	if(argv[1]=="--merge"){
-		getline(cin,input);
-		Sort <unsigned> obj(&input);
 		obj.print_vtr(input);
 		obj.merge(input);
 		obj.print_vtr(input);
 	}
-
+*/
     return 0;
 }
 
